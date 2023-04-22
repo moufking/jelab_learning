907b183ff183e022dd09a927c736eb437fe954c5
Moufid <moufking337@gmail.com>
2023-04-22 15:52:27 +0200

    fix commit

--- resources/views/formation/index.blade.php
+++ resources/views/formation/index.blade.php
@@ -12,12 +12,14 @@ Formation
     <div class="container">
         <h4>Nous avons effectué pour vous une sélection de cours à suivre que vous trouverez ci-dessous ainsi que dans la section Cours suggérés : </h4>
     </div>
-    <div class="row">  
+    <div class="row"> 
         @foreach ($lesFormations as $uneFormation)
         <div class="card-body">
-            <h5 class="card-title">{{Str::upper($uneFormation->titre)}}</h5>
+            <h5 class="card-title">{{Str::upper($uneFormation['titre'])}}</h5>
             <p class="card-text"></p>
         </div>
+        
+        @endforeach
 
     </div>
 </main>
